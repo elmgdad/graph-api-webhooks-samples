af48c22b99025776c6612c2a8cc7ff5a5437c080
elmgdad ibrahim <e.ibrahim@elc.edu.sa>
2024-07-25 14:23:48 +0300

    audio

--- heroku/index.js
+++ heroku/index.js
@@ -1,44 +1,45 @@
-var bodyParser = require('body-parser');
-var express = require('express');
+var bodyParser = require("body-parser");
+var express = require("express");
 var app = express();
-var xhub = require('express-x-hub');
+var xhub = require("express-x-hub");
 const axios = require("axios");
 
+app.set("port", process.env.PORT || 8080);
 
-app.set('port', (process.env.PORT || 8080));
-
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(xhub({ algorithm: "sha1", secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
+var token = process.env.TOKEN || "token";
 var received_updates = [];
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   console.log(req);
-  res.send('main <pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+  res.send("main <pre>" + JSON.stringify(received_updates, null, 2) + "</pre>");
 });
 
-app.get(['/facebook', '/instagram'], function (req, res) {
+app.get(["/facebook", "/instagram"], function (req, res) {
   if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
+    req.query["hub.mode"] == "subscribe" &&
+    req.query["hub.verify_token"] == token
   ) {
-    res.send(req.query['hub.challenge']);
+    res.send(req.query["hub.challenge"]);
   } else {
     res.sendStatus(400);
   }
 });
 
-app.post('/facebook', async function (req, res) {
-  console.log('Facebook request body:', req.body);
+app.post("/facebook", async function (req, res) {
+  console.log("Facebook request body:", req.body);
 
   if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    console.log(
+      "Warning - request header X-Hub-Signature not present or invalid"
+    );
     res.sendStatus(401);
     return;
   }
 
-  console.log('request header X-Hub-Signature validated');
+  console.log("request header X-Hub-Signature validated");
   // Process the Facebook updates here
   received_updates.unshift(req.body);
 
@@ -48,106 +49,57 @@ app.post('/facebook', async function (req, res) {
     let audioId = body_param.entry[0].changes[0].value.messages[0].audio.id;
 
     try {
-      let response = await axios({
-        method: "GET",
-        url: "https://graph.facebook.com/v19.0/" + audioId,
-        headers: {
-          "Content-Type": "application/json",
-          'Authorization': 'Bearer ' + process.env.APP_TOKEN
-        }
-      });
-
-      let response_audio = await axios({
-        method: "GET",
-        url: response.data.url,
-        responseType: 'stream',
-        headers: {
-          "Content-Type": "application/json",
-          'Authorization': 'Bearer ' + process.env.APP_TOKEN
-        }
-      });
+      /* let response = await axios({
+         method: "GET",
+         url: "https://graph.facebook.com/v19.0/" + audioId,
+         headers: {
+           "Content-Type": "application/json",
+           'Authorization': 'Bearer ' + process.env.APP_TOKEN
+         }
+       });
+ 
+       let response_audio = await axios({
+         method: "GET",
+         url: response.data.url,
+         responseType: 'stream',
+         headers: {
+           "Content-Type": "application/json",
+           'Authorization': 'Bearer ' + process.env.APP_TOKEN
+         }
+       });
+       */
 
       let webhock = await axios({
         method: "POST",
-        url: 'https://majexexpress.com/operation/webhook',
+        url: "https://majexexpress.com/operation/webhook",
         headers: {
           "Content-Type": "application/json",
-
         },
-        data : JSON.stringify(req.body)
+        data: JSON.stringify(req.body),
       });
 
-    let config2 = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://graph.facebook.com/v20.0/393297853866738/messages',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + process.env.APP_TOKEN
-      },
-      data: JSON.stringify(webhock.body)
-    };
-
-    axios(config2)
-      .then(response => {
-        console.log('Message sent successfully');
-        res.sendStatus(200);
-      })
-      .catch(error => {
-        console.error('Error sending message:', error);
-        res.sendStatus(500);
-      });
-      
-      
-   /*   let audioFilePath = path.join(__dirname, 'audio.ogg');
-      let writer = fs.createWriteStream(audioFilePath);
-      response_audio.data.pipe(writer);
-
-      writer.on('finish', async () => {
-        const audioBytes = fs.readFileSync(audioFilePath).toString('base64');
-
-        const audioRequest = {
-          audio: {
-            content: audioBytes,
+      let webhockPost = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "https://graph.facebook.com/v20.0/393297853866738/messages",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + process.env.APP_TOKEN,
+        },
+        data: JSON.stringify({
+          messaging_product: "whatsapp",
+          to: from,
+          text: {
+            body: JSON.stringify(webhock.body),
           },
-          config: {
-            encoding: 'OGG_OPUS',
-            sampleRateHertz: 16000,
-            languageCode: 'en-US',
-          },
-        };
+        }),
+      };
 
-        const [transcriptionResponse] = await client.recognize(audioRequest);
-        const transcription = transcriptionResponse.results
-          .map(result => result.alternatives[0].transcript)
-          .join('\n');
-        
-        console.log('Transcription:', transcription);
-
-        // Send response with transcription
-        res.json({
-          message: "Audio received and transcribed",
-          transcription: transcription
-        });
-
-        // Clean up the audio file
-        fs.unlink(audioFilePath, (err) => {
-          if (err) {
-            console.error('Error deleting audio file:', err);
-          }
-        });
-      });
-
-      writer.on('error', (error) => {
-        console.error('Error writing audio file:', error);
-        res.sendStatus(500);
-      });*/
-      
+      axios(webhockPost);
     } catch (error) {
-      console.error('Error processing audio:', error);
+      console.error("Error processing audio:", error);
       res.sendStatus(500);
     }
-
   } else if (body_param.entry[0].changes[0].value.messages[0].type == "text") {
     // Extract information from the webhook request
     let entry = req.body.entry[0];
@@ -159,31 +111,31 @@ app.post('/facebook', async function (req, res) {
     let msg_body = message.text.body;
     // response to user message
     let data = JSON.stringify({
-      "messaging_product": "whatsapp",
-      "to": from,
-      "text": {
-        "body": "your message has been received : " + msg_body
-      }
+      messaging_product: "whatsapp",
+      to: from,
+      text: {
+        body: "your message has been received : " + msg_body,
+      },
     });
 
     let config = {
-      method: 'post',
+      method: "post",
       maxBodyLength: Infinity,
-      url: 'https://graph.facebook.com/v20.0/393297853866738/messages',
+      url: "https://graph.facebook.com/v20.0/393297853866738/messages",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + process.env.APP_TOKEN
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.APP_TOKEN,
       },
-      data: data
+      data: data,
     };
 
     axios(config)
-      .then(response => {
-        console.log('Message sent successfully');
+      .then((response) => {
+        console.log("Message sent successfully");
         res.sendStatus(200);
       })
-      .catch(error => {
-        console.error('Error sending message:', error);
+      .catch((error) => {
+        console.error("Error sending message:", error);
         res.sendStatus(500);
       });
   } else {
@@ -191,14 +143,14 @@ app.post('/facebook', async function (req, res) {
   }
 });
 
-app.post('/instagram', function (req, res) {
-  console.log('Instagram request body:');
+app.post("/instagram", function (req, res) {
+  console.log("Instagram request body:");
   console.log(req.body);
   // Process the Instagram updates here
   received_updates.unshift(req.body);
   res.sendStatus(200);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function () {
+  console.log("Node app is running on port", app.get("port"));
 });
