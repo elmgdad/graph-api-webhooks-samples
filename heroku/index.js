a993a14d08bdf0f63b2238291204618e46ebcb1e
elmgdad ibrahim <e.ibrahim@elc.edu.sa>
2024-07-25 16:52:49 +0300

    update

--- heroku/index.js
+++ heroku/index.js
@@ -47,7 +47,7 @@ app.post("/facebook", async function (req, res) {
     let audioId = body_param.entry[0].changes[0].value.messages[0].audio.id;
     try {
 
-    /*  
+      
       let data = JSON.stringify({
         messaging_product: "whatsapp",
         to: from,
@@ -67,11 +67,12 @@ app.post("/facebook", async function (req, res) {
         data: data,
       };
   
-      await axios(config);      */  
-      let webhookResponse =  axios.get(
-        'https://majexexpress.com/operation/webhook/'+ audioId+"/"+from,
-      
-      );
+      await axios(config).then((response) => {
+        axios.get(
+          'https://majexexpress.com/operation/webhook/'+ audioId+"/"+from,
+        )
+      });        
+      res.sendStatus(200);
 
     } catch (error) {
       console.error("Error processing audio:", error);
